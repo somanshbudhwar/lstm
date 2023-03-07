60a3b18104ab023e1d43b87755c4f6116a365205
doublealiu <46761322+doublealiu@users.noreply.github.com>
2023-03-06 18:57:29 -0800

    update model factory file

--- model_factory.py
+++ model_factory.py
@@ -1,20 +1,23 @@
 from base_model import BaselineEncoderDecoder
 # Build and return the model here based on the configuration.
 from lstm_model import LSTMModel
+from rnn_model import RNNModel
 
 
 def get_model(config_data, vocab):
     hidden_size = config_data['model']['hidden_size']
     embedding_size = config_data['model']['embedding_size']
-    model_type = config_data['model']['model_type']
+    model_type = config_data['model']['model_type'].lower()
     num_layers = config_data['model']['num_layers']
 
     # You may add more parameters if you want
     # model = BaselineEncoderDecoder(hidden_size, embedding_size, vocab, num_layers, model_type)
-    if model_type == "LSTM":
+    if model_type == "lstm":
         model = BaselineEncoderDecoder(hidden_size, embedding_size, vocab, num_layers, model_type)
-    elif model_type == "LSTM2":
+    elif model_type == "lstm2":
         model = LSTMModel(embedding_size, hidden_size, vocab, num_layers)
+    elif model_type == "rnn":
+        model = RNNModel(embedding_size, hidden_size, vocab, num_layers)
     else:
         model = LSTMModel(embedding_size, hidden_size, vocab, num_layers)
     return model
